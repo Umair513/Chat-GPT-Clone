7b0a77943a7570ef6e436355da2edb0578340614
Umair513 <soomroumair513@gmail.com>
2024-03-30 13:26:01 +0500

    Navbar Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { themeSettings } from "./theme";
+import { createTheme } from "@mui/material/styles";
+import { Box, Link, Typography, useTheme } from "@mui/material";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings(), []));
   return (
-    <div>
-      <h1>Umair Ahmed</h1>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline></CssBaseline>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Homepage></Homepage>}></Route>
+          <Route path="/register" element={<Register></Register>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      </ThemeProvider>
+    </>
   );
 }
 
